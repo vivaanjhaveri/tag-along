355b3417956e9989cd346596b88f883d2fdb8524
Vivaan Jhaveri <vivaan.jhaveri@gmail.com>
2025-01-18 18:14:36 -0800

    Updated login functionality

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,48 +1,66 @@
 <template>
     <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <div>
-                <div>
-                    <h3>Login</h3>
-                    <hr />
-                </div>
-                <div class="alert alert-danger" v-if="error">
-                    {{ error }}
-                </div>
-                <form @submit.prevent="onLogin()">
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            v-model.trim="email"
-                        />
-                        <div class="error" v-if="errors.email">
-                            {{ errors.email }}
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            class="form-control"
-                            v-model.trim="password"
-                        />
-                        <div class="error" v-if="errors.password">
-                            {{ errors.password }}
-                        </div>
-                    </div>
-
-                    <div class="my-3">
-                        <button type="submit" class="btn btn-primary">
-                            Login
-                        </button>
-                    </div>
-                </form>
+      <div class="col-md-6 offset-md-3">
+        <div class="card mt-5">
+          <div class="card-body">
+            <h3>Login</h3>
+            <hr />
+            <!-- Error Alert -->
+            <div class="alert alert-danger" v-if="error">
+              {{ error }}
             </div>
+            <form @submit.prevent="onLogin">
+              <div class="form-group mb-3">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  v-model.trim="email"
+                  @blur="validateEmail"
+                  :class="{ 'is-invalid': errors.email }"
+                  placeholder="Enter your email"
+                />
+                <div class="invalid-feedback" v-if="errors.email">
+                  {{ errors.email }}
+                </div>
+              </div>
+              <div class="form-group mb-3">
+                <label for="password">Password</label>
+                <div class="input-group">
+                  <input
+                    :type="showPassword ? 'text' : 'password'"
+                    class="form-control"
+                    id="password"
+                    v-model.trim="password"
+                    @blur="validatePassword"
+                    :class="{ 'is-invalid': errors.password }"
+                    placeholder="Enter your password"
+                  />
+                  <button
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    @click="togglePasswordVisibility"
+                  >
+                    <i :class="showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'"></i>
+                  </button>
+                  <div class="invalid-feedback" v-if="errors.password">
+                    {{ errors.password }}
+                  </div>
+                </div>
+              </div>
+  
+              <div class="my-3">
+                <button type="submit" class="btn btn-primary w-100">
+                  <span>Login</span>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
@@ -62,9 +80,6 @@ export default {
   },
   computed: {
     ...mapGetters('auth', ['isAuthenticated', 'autoLogout']),
-    loading() {
-      return this.$store.state.showLoading;
-    },
   },
   methods: {
     ...mapActions('auth', {
@@ -85,9 +100,6 @@ export default {
         return;
       }
 
-      // Show loading spinner
-      this.$store.commit('SET_LOADING');
-
       // Attempt to login
       try {
         await this.login({
@@ -98,10 +110,7 @@ export default {
         this.$router.push('/dashboard');
       } catch (e) {
         // Display authentication error
-        this.error = e;
-      } finally {
-        // Hide loading spinner
-        this.$store.commit('CLEAR_LOADING');
+        this.error = "Invalid login details.";
       }
     },
 
